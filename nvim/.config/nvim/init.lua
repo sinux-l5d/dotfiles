5b3376628529a108b53b0291bc0bbb88b9888ac8
Simon LEONARD <git-1001af4@sinux.sh>
2025-12-13 12:50:02 +0100

    feat(nvim): use soft tab 2 spaces
    It's not valid for every languages, but that's enough for languages I use nvim for

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -173,6 +173,10 @@ vim.wo.relativenumber = true
 -- Tab options
 vim.o.shiftwidth = 2
 vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.expandtab = true
+vim.o.autoindent = true
+vim.o.smartindent = true
 
 
 -- Enable mouse mode
